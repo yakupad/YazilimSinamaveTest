f77a5bb605e028a0d13b493cb889e6fd42c08254
Raşit Şahin <rasit.sahin@kocsistem.com.tr>
2016-12-27 17:01:37 +0300

    sildim

--- YazilimSinamaveTest/frmUyeGiris.Designer.cs
+++ YazilimSinamaveTest/frmUyeGiris.Designer.cs
@@ -36,8 +36,6 @@
             this.btnKayitOl = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
@@ -108,8 +106,6 @@
             // 
             this.groupBox1.BackColor = System.Drawing.Color.Transparent;
             this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.groupBox1.Controls.Add(this.dateTimePicker1);
-            this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.btnKayitOl);
             this.groupBox1.Controls.Add(this.btnGiris);
@@ -134,22 +130,6 @@
             this.label1.Size = new System.Drawing.Size(301, 89);
             this.label1.TabIndex = 10;
             this.label1.Text = "NONAME PROJECT MANAGEMENT";
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(95, 107);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker1.TabIndex = 11;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(188, 130);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // frmUyeGiris
             // 
@@ -185,7 +165,5 @@
         private System.Windows.Forms.Button btnKayitOl;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.Button button1;
     }
 }
