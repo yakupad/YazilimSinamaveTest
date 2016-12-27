5c9eed0f305c88ec7d6f3a3e87dbe92e7481206e
Raşit Şahin <rasit.sahin@kocsistem.com.tr>
2016-12-27 16:56:06 +0300

    ghg

--- YazilimSinamaveTest/frmUyeGiris.Designer.cs
+++ YazilimSinamaveTest/frmUyeGiris.Designer.cs
@@ -135,7 +135,7 @@
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(200, 106);
+            this.dateTimePicker1.Location = new System.Drawing.Point(95, 107);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
             this.dateTimePicker1.TabIndex = 11;
